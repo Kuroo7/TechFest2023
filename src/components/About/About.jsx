686a16fbcd8ac1cf0406403d7be4796bd47d31fb
Mohd Ayan Beg <MohdAyanBeg>
2023-11-27 04:13:35 +0530

    about added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,14 +5,16 @@ export const About = () => {
     <>
         <div className='about'>
             <div className="left">
-                <h1>ABOUT TECHFEST</h1>
-                <p>TechFest is a two-day fiesta that calls in for various technical, egaming, and photography events, wrapped up with an exclusive event for each day. ABES has been organizing TechFest for over a years now, and with a foot fall of 2000+ students from different colleges in Delhi NCR, TechFest has been a success story</p>
-            <div className="buttons">
-                <button>EVENTS</button>
-                <button>BUY NOW</button></div>
+                <h1 className="greenNeon">ABOUT TECHFEST</h1>
+                <p className="white">TechFest is a two-day fiesta that calls in for various technical, egaming, and photography events, wrapped up with an exclusive event for each day. ABES has been organizing TechFest for over a years now, and with a foot fall of 2000+ students from different colleges in Delhi NCR, TechFest has been a success story</p>
+                
+                <div className="buttons">
+                <button className="events">EVENTS</button>
+                <button className="buy">BUY NOW</button>
+                </div>
             </div>
             <div className="right">
-                <img src="/src/assets/logo.png" alt="logo" />
+                <img src="/src/assets/logoWithoutBG.png" alt="logo" />
             </div>
             
             

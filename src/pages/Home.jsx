0dd6d6405fd9bfca118a7ec543764ddc0e20f93d
Mohd Ayan Beg <MohdAyanBeg>
2023-11-27 04:23:00 +0530

    hero updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 
-// import { About } from "../components/About/About"
+import { About } from "../components/About/About"
 
 import Hero from "../components/Hero/Hero"
 import PastEvents from "../components/PastEvents/PastEvents"
@@ -10,7 +10,7 @@ export const Home = () => {
     return (
         <>
             <Hero />
-            {/* <About/> */}
+            <About/>
             <PastEvents />
 
 

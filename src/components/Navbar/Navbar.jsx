2293a87e264b2f17ef6e6c5d686ddd923e7ed6c8
Mohammad Atif Shameem Khan <atif40666@gmail.com>
2023-11-27 02:11:44 +0530

    Navbar updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom"
+import logo from "../../assets/logo.png"
+import "./navbar.css"
 const Navbar = () => {
     return (
         <nav className="Navbar">
-            <div>
-                <img src="#" alt="lol" />
+            <div className="logo-container" >
+                <img className="logo" src={logo} alt="lol" />
             </div>
-            <div>
-                <li>Home</li>
-                <li>Home</li>
-                <Link to="/" >Home</Link >
+            <div className="nav-items" >
+                <Link className="nav-links" to="/" >Home</Link >
+                <Link className="nav-links" to="/" >About</Link >
+                <Link className="nav-links" to="/" >Past</Link >
+                <Link className="nav-links" to="/" >This Year</Link >
+                <Link className="nav-links" to="/" >FAQ</Link >
+                <Link className="nav-links" to="/" >Team</Link >
             </div>
 
         </nav>

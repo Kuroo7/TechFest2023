0fb8860a8fd0af5dd262bc2f36769fc3e5364971
Mohammad Atif Shameem Khan <atif40666@gmail.com>
2023-11-28 12:34:26 +0530

    footer Updated

--- src/components/PastEvents/PastEvents.jsx
+++ src/components/PastEvents/PastEvents.jsx
@@ -10,7 +10,15 @@ const PastEvents = () => {
                             Know about the past.
                         </h1>
                         <p>
-                            ABES's annual cultural fest is one for those who love and enjoy entertainment. This festival is a treat for music, culture, and theatre art lovers. It has popular events that are not only entertaining but also make you win goodies and exciting cash prizes. Music enthusiasts and dance lovers can never miss this fest, as they have seen the likes of Guru Randhawa, Nikhil D'Souza, Jassie Gill, and Babbal Rai perform here. With its exuberant atmosphere and liveliness, this festival is a full packet of memories and new experiences.
+                            ABES annual cultural fest is one for those who love and
+                            enjoy entertainment. This festival is a treat for music,
+                            culture, and theatre art lovers. It has popular events
+                            that are not only entertaining but also make you win goodies and
+                            exciting cash prizes. Music enthusiasts and dance lovers can never
+                            miss this fest, as they have seen the likes of Guru Randhawa, Nikhil
+                            D'Souza, Jassie Gill, and Babbal Rai perform here.
+                            With its exuberant atmosphere and liveliness,
+                            this festival is a full packet of memories and new experiences.
                         </p>
                         <button className="PEbutton">
                             View Gallery

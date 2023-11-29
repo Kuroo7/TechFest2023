391fb304d2d843ea3904e9eb80491d87d1259751
Mohd Ayan Beg <MohdAyanBeg>
2023-11-29 23:02:15 +0530

    scrollbar and about page logo updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,7 @@ export const About = () => {
                 <div className="about-container">
                     <div className="left">
                         <h1 className="greenNeon" >
-                            ABOUT TECHFEST
+                            About Techfest
                         </h1>
                         <p className="white" >
                             TechFest is a two-day fiesta that calls in for various technical, egaming, and photography events, wrapped up with an exclusive event for each day. ABES has been organizing TechFest for over a years now, and with a foot fall of 2000+ students from different colleges in Delhi NCR, TechFest has been a success story

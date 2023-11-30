6d6754c97a166b0807fc4db9f3ed6fbda4917eab
ISHANVI CHAUHAN <ishanvi.chauhan@gmail.com>
2023-11-30 19:58:38 +0530

    Updated Event Carousel Section

--- src/components/EventsCarousel/EventCarousel.jsx
+++ src/components/EventsCarousel/EventCarousel.jsx
@@ -5,7 +5,146 @@ const EventCarousel = () => {
 
     return (
         <>
-            {/* Corousel Html Goes here */}
+            <div className="carousel">
+                <div className="slider1">
+                    <div className="slide-track">
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="slider2">
+                    <div className="slide-track">
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="slider3">
+                    <div className="slide-track">
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                        <div className="slide">
+                            <img src="/src/assets/sample.jpeg"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }

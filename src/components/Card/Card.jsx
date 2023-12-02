ed7d2259726ca0727c379859bde52de6ed70c8fc
ISHANVI CHAUHAN <ishanvi.chauhan@gmail.com>
2023-12-02 17:34:26 +0530

    Team Cards Updated

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,57 @@
 import "./card.css"
 export const Card = () => {
     return (
-        <div>Card</div>
+        <div className="card">
+            <div className="main-card">
+                <div className="back"></div>
+                <div className="photo">
+                    <div className="circle">
+                        <img src="sample.jpeg" alt=""></img>
+                    </div>
+                </div>
+                <div className="info">
+                    <h3>Lorem Ipsum</h3>
+                    <p className="post">General Secretary</p>
+                    <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem assumenda dolores adipisci.</p>
+                    <p className="contact">+91 12345 67890</p>
+                    <a href="#"><i className="fa-brands fa-linkedin"></i></a>
+                    <a href="#"><i className="fa-brands fa-instagram"></i></a>
+                </div>
+            </div>
+
+            <div className="main-card">
+                <div className="back"></div>
+                <div className="photo">
+                    <div className="circle">
+                        <img src="sample.jpeg" alt=""></img>
+                    </div>
+                </div>
+                <div className="info">
+                    <h3>Lorem Ipsum</h3>
+                    <p className="post">General Secretary</p>
+                    <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem assumenda dolores adipisci.</p>
+                    <p className="contact">+91 12345 67890</p>
+                    <a href="#"><i className="fa-brands fa-linkedin"></i></a>
+                    <a href="#"><i className="fa-brands fa-instagram"></i></a>
+                </div>
+            </div>
+            
+            <div className="main-card">
+                <div className="back"></div>
+                <div className="photo">
+                    <div className="circle">
+                        <img src="sample.jpeg" alt=""></img>
+                    </div>
+                </div>
+                <div className="info">
+                    <h3>Lorem Ipsum</h3>
+                    <p className="post">General Secretary</p>
+                    <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem assumenda dolores adipisci.</p>
+                    <p className="contact">+91 12345 67890</p>
+                    <a href="#"><i className="fa-brands fa-linkedin"></i></a>
+                    <a href="#"><i className="fa-brands fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
     )
 }

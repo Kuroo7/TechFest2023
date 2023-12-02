1a8b6432e746c0cfdd62ff9e42185efe6f7f9aaf
ISHANVI CHAUHAN <ishanvi.chauhan@gmail.com>
2023-12-02 23:21:32 +0530

    Team Cards Updated

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,85 +1,105 @@
 import "./card.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 export const Card = () => {
     return (
-        <div className="team-card">
-            <div className="main-card">
-                <div className="back"></div>
-                <div className="photo">
-                    <div className="circle">
-                        <img src="sample.jpeg" alt=""></img>
+        <div className="all-cards">
+            <div className="head-card">
+                <div className="main-card">
+                        <div className="back"></div>
+                        <div className="photo">
+                            <div className="circle">
+                                <img src="/src/assets/sample.jpeg" alt=""></img>
+                            </div>
+                        </div>
+                        <div className="info">
+                            <h3>Lorem Ipsum</h3>
+                            <p className="post">General Secretary</p>
+                            <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem assumenda dolores adipisci.</p>
+                            <p className="contact">+91 12345 67890</p>
+                            <div className="social">
+                                <a href="#"><FontAwesomeIcon className=' socialIcon' icon={faLinkedin} /></a>
+                                <a href="#"><FontAwesomeIcon className=' socialIcon' icon={faInstagram} /></a>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="info">
-                    <h3>Lorem Ipsum</h3>
-                    <p className="post">General Secretary</p>
-                    <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem assumenda dolores adipisci.</p>
-                    <p className="contact">+91 12345 67890</p>
-                    <a href="#"><i className="fa-brands fa-linkedin"></i></a>
-                    <a href="#"><i className="fa-brands fa-instagram"></i></a>
-                </div>
             </div>
-            <div className="main-card">
-                <div className="back"></div>
-                <div className="photo">
-                    <div className="circle">
-                        <img src="sample.jpeg" alt=""></img>
+
+            <div className="team-cards">
+                <div className="main-card">
+                    <div className="back"></div>
+                    <div className="photo">
+                        <div className="circle">
+                            <img src="/src/assets/sample.jpeg" alt=""></img>
+                        </div>
+                    </div>
+                    <div className="info">
+                        <h3>Lorem Ipsum</h3>
+                        <p className="post">General Secretary</p>
+                        <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem assumenda dolores adipisci.</p>
+                        <p className="contact">+91 12345 67890</p>
+                        <div className="social">
+                            <a href="#"><FontAwesomeIcon className=' socialIcon' icon={faLinkedin} /></a>
+                            <a href="#"><FontAwesomeIcon className=' socialIcon' icon={faInstagram} /></a>
+                        </div>
                     </div>
                 </div>
-                <div className="info">
-                    <h3>Lorem Ipsum</h3>
-                    <p className="post">General Secretary</p>
-                    <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem assumenda dolores adipisci.</p>
-                    <p className="contact">+91 12345 67890</p>
-                    <a href="#"><i className="fa-brands fa-linkedin"></i></a>
-                    <a href="#"><i className="fa-brands fa-instagram"></i></a>
-                </div>
-            </div>
-            <div className="main-card">
-                <div className="back"></div>
-                <div className="photo">
-                    <div className="circle">
-                        <img src="sample.jpeg" alt=""></img>
+                <div className="main-card">
+                    <div className="back"></div>
+                    <div className="photo">
+                        <div className="circle">
+                            <img src="/src/assets/sample.jpeg" alt=""></img>
+                        </div>
+                    </div>
+                    <div className="info">
+                        <h3>Lorem Ipsum</h3>
+                        <p className="post">General Secretary</p>
+                        <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem assumenda dolores adipisci.</p>
+                        <p className="contact">+91 12345 67890</p>
+                        <div className="social">
+                            <a href="#"><FontAwesomeIcon className=' socialIcon' icon={faLinkedin} /></a>
+                            <a href="#"><FontAwesomeIcon className=' socialIcon' icon={faInstagram} /></a>
+                        </div>
                     </div>
                 </div>
-                <div className="info">
-                    <h3>Lorem Ipsum</h3>
-                    <p className="post">General Secretary</p>
-                    <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem assumenda dolores adipisci.</p>
-                    <p className="contact">+91 12345 67890</p>
-                    <a href="#"><i className="fa-brands fa-linkedin"></i></a>
-                    <a href="#"><i className="fa-brands fa-instagram"></i></a>
-                </div>
-            </div>
-            <div className="main-card">
-                <div className="back"></div>
-                <div className="photo">
-                    <div className="circle">
-                        <img src="sample.jpeg" alt=""></img>
+                <div className="main-card">
+                    <div className="back"></div>
+                    <div className="photo">
+                        <div className="circle">
+                            <img src="/src/assets/sample.jpeg" alt=""></img>
+                        </div>
+                    </div>
+                    <div className="info">
+                        <h3>Lorem Ipsum</h3>
+                        <p className="post">General Secretary</p>
+                        <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem assumenda dolores adipisci.</p>
+                        <p className="contact">+91 12345 67890</p>
+                        <div className="social">
+                            <a href="#"><FontAwesomeIcon className=' socialIcon' icon={faLinkedin} /></a>
+                            <a href="#"><FontAwesomeIcon className=' socialIcon' icon={faInstagram} /></a>
+                        </div>
                     </div>
                 </div>
-                <div className="info">
-                    <h3>Lorem Ipsum</h3>
-                    <p className="post">General Secretary</p>
-                    <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem assumenda dolores adipisci.</p>
-                    <p className="contact">+91 12345 67890</p>
-                    <a href="#"><i className="fa-brands fa-linkedin"></i></a>
-                    <a href="#"><i className="fa-brands fa-instagram"></i></a>
-                </div>
-            </div>
-            <div className="main-card">
-                <div className="back"></div>
-                <div className="photo">
-                    <div className="circle">
-                        <img src="sample.jpeg" alt=""></img>
+                <div className="main-card">
+                    <div className="back"></div>
+                    <div className="photo">
+                        <div className="circle">
+                            <img src="/src/assets/sample.jpeg" alt=""></img>
+                        </div>
                     </div>
-                </div>
-                <div className="info">
-                    <h3>Lorem Ipsum</h3>
-                    <p className="post">General Secretary</p>
-                    <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem assumenda dolores adipisci.</p>
-                    <p className="contact">+91 12345 67890</p>
-                    <a href="#"><i className="fa-brands fa-linkedin"></i></a>
-                    <a href="#"><i className="fa-brands fa-instagram"></i></a>
+                    <div className="info">
+                        <h3>Lorem Ipsum</h3>
+                        <p className="post">General Secretary</p>
+                        <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem assumenda dolores adipisci.</p>
+                        <p className="contact">+91 12345 67890</p>
+                        <div className="social">
+                            <a href="#"><FontAwesomeIcon className=' socialIcon' icon={faLinkedin} /></a>
+                            <a href="#"><FontAwesomeIcon className=' socialIcon' icon={faInstagram} /></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

f84e28c00706b54d808f45ba5eb200cea5f6831e
Jagrati Sharma <j.sharma1416@gmail.com>
2023-12-03 23:48:58 +0530

    Carousel is updated

--- src/components/EventsCarousel/EventCarousel.jsx
+++ src/components/EventsCarousel/EventCarousel.jsx
@@ -11,75 +11,75 @@ const EventCarousel = () => {
                     <div className="slider1">
                         <div className="slide-track">
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/1_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/2_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/3_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/4_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/5_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/6_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/7_eve.jpg" />
                             </div>
                         </div>
                     </div>
                     <div className="slider2">
                         <div className="slide-track">
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/8_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/9_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/10_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/11_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/12_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/13_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/14_eve.jpg" />
                             </div>
                         </div>
                     </div>
                     <div className="slider3">
                         <div className="slide-track">
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/15_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/16_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/17_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/18_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/1_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/2_eve.jpg" />
                             </div>
                             <div className="slide">
-                                <img src="/src/assets/sample.jpeg" />
+                                <img src="/src/assets/3_eve.jpg" />
                             </div>
                         </div>
                     </div>

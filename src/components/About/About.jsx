318db884b8c9899c4e828320736bb7fa681e3d2f
Mohammad Atif Shameem Khan <atif40666@gmail.com>
2023-12-03 22:28:43 +0530

    Event card and Logo updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,13 +7,17 @@ export const About = () => {
                 <div className="about-container">
                     <div className="left">
                         <h1 className="greenNeon" >
-                            About Techfest
+                            About TechFest
                         </h1>
                         <p className="white" >
                             TechFest is a two-day fiesta that calls in for various technical, egaming, and photography events, wrapped up with an exclusive event for each day. ABES has been organizing TechFest for over a years now, and with a foot fall of 2000+ students from different colleges in Delhi NCR, TechFest has been a success story
                         </p>
                         <div className="buttons">
-                            <button className="events " >EVENTS</button>
+                            <button className="events" >
+                                <a href="#events_carousel">
+                                    EVENTS
+                                </a>
+                            </button>
                             <button className="buy " >BUY NOW</button>
                         </div>
                     </div>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,31 +1,31 @@
 import "./card.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faInstagram,
-  faLinkedin,
+    faInstagram,
+    faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
 export const Card = () => {
     return (
         <div className="all-cards">
             <div className="head-card">
                 <div className="main-card">
-                        <div className="back"></div>
-                        <div className="photo">
-                            <div className="circle">
-                                <img src="/src/assets/sample.jpeg" alt=""></img>
-                            </div>
-                        </div>
-                        <div className="info">
-                            <h3>Lorem Ipsum</h3>
-                            <p className="post">General Secretary</p>
-                            <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem assumenda dolores adipisci.</p>
-                            <p className="contact">+91 12345 67890</p>
-                            <div className="social">
-                                <a href="#"><FontAwesomeIcon className=' socialIcon' icon={faLinkedin} /></a>
-                                <a href="#"><FontAwesomeIcon className=' socialIcon' icon={faInstagram} /></a>
-                            </div>
+                    <div className="back"></div>
+                    <div className="photo">
+                        <div className="circle">
+                            <img src="/src/assets/sample.jpeg" alt=""></img>
                         </div>
                     </div>
+                    <div className="info">
+                        <h3>Lorem Ipsum</h3>
+                        <p className="post">General Secretary</p>
+                        <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem assumenda dolores adipisci.</p>
+                        <p className="contact">+91 12345 67890</p>
+                        <div className="social">
+                            <a href="#"><FontAwesomeIcon className=' socialIcon' icon={faLinkedin} /></a>
+                            <a href="#"><FontAwesomeIcon className=' socialIcon' icon={faInstagram} /></a>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div className="team-cards">
@@ -83,7 +83,7 @@ export const Card = () => {
                         </div>
                     </div>
                 </div>
-                <div className="main-card">
+                {/* <div className="main-card">
                     <div className="back"></div>
                     <div className="photo">
                         <div className="circle">
@@ -100,7 +100,7 @@ export const Card = () => {
                             <a href="#"><FontAwesomeIcon className=' socialIcon' icon={faInstagram} /></a>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     )

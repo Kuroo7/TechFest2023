38d5976f3b550046cd783f7ce158d1a8109921e5
Jagrati Sharma <j.sharma1416@gmail.com>
2023-12-05 22:51:09 +0530

    Carousel is updated

--- src/components/EventsCarousel/EventCarousel.jsx
+++ src/components/EventsCarousel/EventCarousel.jsx
@@ -1,3 +1,22 @@
+import img1 from "../../assets/1_eve.jpg"
+import img2 from "../../assets/2_eve.jpg"
+import img3 from "../../assets/3_eve.jpg"
+import img4 from "../../assets/4_eve.jpg"
+import img5 from "../../assets/5_eve.jpg"
+import img6 from "../../assets/6_eve.jpg"
+import img7 from "../../assets/7_eve.jpg"
+import img8 from "../../assets/8_eve.jpg"
+import img9 from "../../assets/9_eve.jpg"
+import img10 from "../../assets/10_eve.jpg"
+import img11 from "../../assets/11_eve.jpg"
+import img12 from "../../assets/12_eve.jpg"
+import img13 from "../../assets/13_eve.jpg"
+import img14 from "../../assets/14_eve.jpg"
+import img15 from "../../assets/15_eve.jpg"
+import img16 from "../../assets/16_eve.jpg"
+import img17 from "../../assets/17_eve.jpg"
+import img18 from "../../assets/18_eve.jpg"
+
 
 import "./EventCarousel.css"
 

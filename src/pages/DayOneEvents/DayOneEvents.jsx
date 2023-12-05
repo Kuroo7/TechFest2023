d3ac2400e5089d590ade8c1cdf5eeaa5b062ae62
Mayank Srivastava <srimayank2002@gmail.com>
2023-12-05 23:46:52 +0530

    day1event card

--- src/pages/DayOneEvents/DayOneEvents.jsx
+++ src/pages/DayOneEvents/DayOneEvents.jsx
@@ -2,7 +2,24 @@ import "./dayOneEvents.css"
 const DayOneEvents = () => {
     return (
         <>
-            <div className="DayOne-container">DayOneEvents</div>
+            <div className="DayOne-container">
+                <div className="ecard1">
+                    <div className="eimage">
+                        <img src="/src/assets/robo.jpg"/>
+                    </div>
+                    <div className="ename">
+                        <p><img src="/src/assets/staricon.png" alt="" className="star"/></p>
+                        <p className="eventname">Roborace</p>
+                        <p><img src="/src/assets/staricon.png" alt="" className="star"/></p>
+                    </div>
+                    <hr className="hr"/>
+                    <div className="but">
+                        <button className="register">Register</button>
+                        {/* <div className="vertical"></div> */}
+                        <button className="explore">Explore</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }

ace888cb3fa5a88710b358364802b2940edd0ed3
ISHANVI CHAUHAN <ishanvi.chauhan@gmail.com>
2024-01-13 15:49:28 +0530

    Added events 18 and 19 in DayTwoData.js file

--- src/pages/DayTwoDetails/DayTwoData.js
+++ src/pages/DayTwoDetails/DayTwoData.js
@@ -19,5 +19,50 @@ const dayTwoData = [
         // redirectLink: "/cultural/NrutyamS",
         redirectLink: "/Day1/RoboRace",
     },
+    {
+        id: 18,
+        name: "ELTiro",
+        imgSrc: "#",
+        heading: "EL Tiro",
+        venue: "TBD",
+        headingPara: "El Tiro to combine science, engineering, and fun by tasking participants with designing, building, and launching rockets propelled solely by water pressure. This event offers a hands-on experience in rocketry and aerodynamics.",
+        rules: [
+            "1. Respectful behavior and cooperation among participants are expected at all times.",
+            "2. Any disputes or disagreements should be brought to the attention of event organizers for resolution.",
+            "3. Participants must demonstrate sportsmanship and fair play throughout the event.",
+            "4. Any attempts to sabotage other participants' rockets will result in immediate disqualification."
+        ],
+        judgingCriteria: [
+            "Flight Distance",
+            "Accuracy",
+            "Design",
+            "Safety"
+        ],
+        spocDetails: '',
+        paymentLink: "",
+        // redirectLink: "/cultural/NrutyamS",
+        redirectLink: "/Day1/ELTiro",
+    },
+    {
+        id: 19,
+        name: "WebsiteGeneration",
+        imgSrc: "#",
+        heading: "Website Generation",
+        venue: "TBD",
+        headingPara: "The Website generation aims to explore the capabilities of artificial intelligence in website creation. Participants will harness AI tools to generate, design, and optimize websites for specific purposes or industries within a limited time frame.",
+        rules: [
+            "1. Participants must bring their own devices for the presentation.",
+            "2. Participants can register individually or as a team.",
+            "3. Respectful and inclusive behaviour is expected from all participants. Any form of cheating, plagiarism or unethical behaviour will result in instant disqualification.",
+        ],
+        judgingCriteria: [
+            "-",
+            "-"
+        ],
+        spocDetails: '',
+        paymentLink: "",
+        // redirectLink: "/cultural/NrutyamS",
+        redirectLink: "/Day1/RoboRace",
+    },
 ]
 export default dayTwoData
